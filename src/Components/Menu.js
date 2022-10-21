f367c75a4a859c0ab555900a33a1c58e83b23fea
Jennifer Cao <jennifercao291953@berkeley.edu>
2022-10-20 19:32:37 -0700

    finish q3

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,13 +1,15 @@
 import React from 'react';
 /* Add any imports you think you might need here! */
 
-const Menu = () => { 
-
-    return (
-      <div className="colorOptions">
-          {/* TODO */}
-      </div>
-    );
+const Menu = (props) => { 
+  return (
+    <div className="colorOptions">
+        <Color color="red" handleClick={() => props.createPost("red")}></Color>
+        <Color color="pink" handleClick={() => props.createPost("pink")}></Color>
+        <Color color="blue" handleClick={() => props.createPost("blue")}></Color>
+        <Color color="green" handleClick={() => props.createPost("green")}></Color>
+    </div>
+  );
 }
 
 export default Menu;
